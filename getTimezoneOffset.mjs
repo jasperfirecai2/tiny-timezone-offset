612829f96676dcffc6bf9b9d7f0a5213f566aa59
Jasper <j.borsboom@veldwerk.nl>
2024-04-17 17:01:13 +0200

    Initial version

---
+++ getTimezoneOffset.mjs
@@ -0,0 +1,12 @@
+export function getTimezoneOffset(date, destinationTimezoneName) {
+    const offSetFormatter = new Intl.DateTimeFormat("nl", {hour:"numeric", timeZoneName:"shortOffset", timeZone: destinationTimezoneName});
+    const offsetString = offSetFormatter.format(date).split("GMT")[1];
+    if (offsetString.includes(":")) {
+        const splitString = offsetString.split(":");
+        const hourOffset = parseInt(splitString[0]);
+        const minuteOffset = parseInt(splitString[1]);
+      return {hourOffset, minuteOffset, totalOffset: hourOffset * 60 + minuteOffset};
+    }
+    const hourOffset = parseInt(offsetString);
+    return {hourOffset, minuteOffset: 0, totalOffset: hourOffset * 60};
+}
